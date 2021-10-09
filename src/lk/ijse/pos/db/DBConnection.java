d8c6f3e3adc1825de114ca3eb71f09d5ca7b3518
SudharaSathyavi <sudarasathyavi6436@gmail.com>
2021-10-09 12:08:45 +0530

    create DBConnection

--- src/lk/ijse/pos/db/DBConnection.java
+++ src/lk/ijse/pos/db/DBConnection.java
@@ -9,9 +9,32 @@
 
 package lk.ijse.pos.db;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  * @author Sudhara Sathyavi <sudarasathyavi6436@gmail.com>sudhara sathyavi
  * @since 10/9/2021
  */
 public class DBConnection {
+    private static DBConnection dBConnection;
+    private Connection connection;
+
+    public DBConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/pos", "root", "9999");
+    }
+
+    public static DBConnection getInstance() throws ClassNotFoundException, SQLException {
+        if (dBConnection == null) {
+            return dBConnection = new DBConnection();
+        } else {
+            return dBConnection;
+        }
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
 }
