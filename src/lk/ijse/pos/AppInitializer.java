1886d339c5fec09d6c37d7b99adba07a0a951c20
SudharaSathyavi <sudarasathyavi6436@gmail.com>
2021-10-09 10:24:02 +0530

    initialized project

---
+++ src/lk/ijse/pos/AppInitializer.java
@@ -0,0 +1,29 @@
+/*
+ *
+ *  * ---------------------------------------------------------------------------------------------
+ *  *  *  Copyright (c)GDSE-55-Batch. All rights reserved.
+ *  *  *  Licensed under the SriLankan Information License. See License.txt in the project root for license information.
+ *  *  *--------------------------------------------------------------------------------------------
+ *
+ */
+
+package lk.ijse.pos;
+/**
+ * @author Sudhara Sathyavi <sudarasathyavi6436@gmail.com>sudhara sathyavi
+ * @since 10/9/2021
+ */
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class AppInitializer extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
---
+++ src/lk/ijse/pos/controller/CustomerFormController.java
@@ -0,0 +1,17 @@
+/*
+ *
+ *  * ---------------------------------------------------------------------------------------------
+ *  *  *  Copyright (c)GDSE-55-Batch. All rights reserved.
+ *  *  *  Licensed under the SriLankan Information License. See License.txt in the project root for license information.
+ *  *  *--------------------------------------------------------------------------------------------
+ *
+ */
+
+package lk.ijse.pos.controller;
+
+/**
+ * @author Sudhara Sathyavi <sudarasathyavi6436@gmail.com>sudhara sathyavi
+ * @since 10/9/2021
+ */
+public class CustomerFormController {
+}
