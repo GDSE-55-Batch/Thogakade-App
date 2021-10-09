383ecb52cf34c9ca275f3b17f4e7d8b9cf46c7f4
SudharaSathyavi <sudarasathyavi6436@gmail.com>
2021-10-09 12:45:05 +0530

    create ItemController

--- src/lk/ijse/pos/controller/ItemFormController.java
+++ src/lk/ijse/pos/controller/ItemFormController.java
@@ -9,9 +9,26 @@
 
 package lk.ijse.pos.controller;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 /**
  * @author Sudhara Sathyavi <sudarasathyavi6436@gmail.com>sudhara sathyavi
  * @since 10/9/2021
  */
 public class ItemFormController {
+
+    @Override
+    public boolean saveItem(Item i) throws SQLException, ClassNotFoundException {
+        Connection con= DbConnection.getInstance().getConnection();
+        String query="INSERT INTO Item VALUES(?,?,?,?,?)";
+        PreparedStatement stm = con.prepareStatement(query);
+        stm.setObject(1,i.getItemID());
+        stm.setObject(2,i.getItenDescription());
+        stm.setObject(3,i.getPackSize());
+        stm.setObject(4,i.getUnitPrice());
+        stm.setObject(5,i.getQtyOnHand());
+        return stm.executeUpdate()>0;
+    }
 }
